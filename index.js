6dc2ed3db796b447d8eb2b5233c37d1e6fb7b616
Aakash Shah <bca64sem1ddu@gmail.com>
2023-02-06 10:22:57 +0530

    f

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 import express from 'express';
-
+import Web3 from 'web3';
 const app = express();
 
 const port = 9000;
+
+app.get('/create-wallet', (req, res) => {
+    const web3 = new Web3();
+    const wallet = web3.eth.accounts.create();
+    res.json(wallet);
+});
 
 app.listen(9000,()=>{
     console.log("Started");
